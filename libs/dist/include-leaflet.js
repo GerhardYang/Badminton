c62efd91468f92ce8eea2f2b6d45c062a0155863
光辉 <gh.yang@outlook.com>
2018-11-24 17:26:49 +0800

    https

--- libs/dist/include-leaflet.js
+++ libs/dist/include-leaflet.js
@@ -36,17 +36,17 @@
         var includes = (targetScript.getAttribute('include') || "").split(",");
         var excludes = (targetScript.getAttribute('exclude') || "").split(",");
         if (!inArray(excludes, 'leaflet')) {
-            inputCSS("http://cdn.bootcss.com/leaflet/1.2.0/leaflet.css");
-            inputScript("http://cdn.bootcss.com/leaflet/1.2.0/leaflet.js");
+            inputCSS("https://cdn.bootcss.com/leaflet/1.2.0/leaflet.css");
+            inputScript("https://cdn.bootcss.com/leaflet/1.2.0/leaflet.js");
         }
         if (inArray(includes, 'mapv')) {
-            inputScript("http://mapv.baidu.com/build/mapv.min.js");
+            inputScript("https://mapv.baidu.com/build/mapv.min.js");
         }
         if (inArray(includes, 'turf')) {
             inputScript("https://cdn.bootcss.com/Turf.js/4.6.1/turf.min.js");
         }
         if (inArray(includes, 'echarts')) {
-            inputScript("http://cdn.bootcss.com/echarts/3.8.0/echarts.min.js");
+            inputScript("https://cdn.bootcss.com/echarts/3.8.0/echarts.min.js");
         }
         if (inArray(includes, 'd3')) {
             inputScript("https://cdn.bootcss.com/d3/4.10.2/d3.min.js");
@@ -55,10 +55,10 @@
             inputScript("https://d3js.org/d3-hexbin.v0.2.min.js");
         }
         if (inArray(includes, 'd3Layer')) {
-            inputScript("http://iclient.supermap.io/libs/leaflet/plugins/leaflet.d3Layer/leaflet-d3Layer.min.js");
+            inputScript("https://iclient.supermap.io/libs/leaflet/plugins/leaflet.d3Layer/leaflet-d3Layer.min.js");
         }
         if (inArray(includes, 'elasticsearch')) {
-            inputScript("http://cdn.bootcss.com/elasticsearch/13.0.1/elasticsearch.min.js");
+            inputScript("https://cdn.bootcss.com/elasticsearch/13.0.1/elasticsearch.min.js");
         }
         if (!inArray(excludes, 'iclient9-leaflet')) {
             inputScript("./libs/dist/iclient9-leaflet.min.js");
@@ -67,27 +67,27 @@
             inputCSS("./libs/dist/iclient9-leaflet.min.css");
         }
         if (inArray(includes, 'leaflet.heat')) {
-            inputScript("http://cdn.bootcss.com/leaflet.heat/0.2.0/leaflet-heat.js");
+            inputScript("https://cdn.bootcss.com/leaflet.heat/0.2.0/leaflet-heat.js");
         }
         if (inArray(includes, 'osmbuildings')) {
             inputScript("http://iclient.supermap.io/libs/osmbuildings/OSMBuildings-Leaflet.js");
         }
         if (inArray(includes, 'leaflet.markercluster')) {
-            inputCSS("http://cdn.bootcss.com/leaflet.markercluster/1.0.3/MarkerCluster.Default.css");
-            inputCSS("http://cdn.bootcss.com/leaflet.markercluster/1.0.3/MarkerCluster.css");
-            inputScript("http://cdn.bootcss.com/leaflet.markercluster/1.0.3/leaflet.markercluster.js");
+            inputCSS("https://cdn.bootcss.com/leaflet.markercluster/1.0.3/MarkerCluster.Default.css");
+            inputCSS("https://cdn.bootcss.com/leaflet.markercluster/1.0.3/MarkerCluster.css");
+            inputScript("https://cdn.bootcss.com/leaflet.markercluster/1.0.3/leaflet.markercluster.js");
         }
         if (inArray(includes, 'leaflet-icon-pulse')) {
-            inputCSS("http://iclient.supermap.io/libs/leaflet/plugins/leaflet-icon-pulse/L.Icon.Pulse.css");
-            inputScript("http://iclient.supermap.io/libs/leaflet/plugins/leaflet-icon-pulse/L.Icon.Pulse.js");
+            inputCSS("https://iclient.supermap.io/libs/leaflet/plugins/leaflet-icon-pulse/L.Icon.Pulse.css");
+            inputScript("https://iclient.supermap.io/libs/leaflet/plugins/leaflet-icon-pulse/L.Icon.Pulse.js");
         }
         if (inArray(includes, 'leaflet.draw')) {
-            inputCSS("http://cdn.bootcss.com/leaflet.draw/0.4.12/leaflet.draw.css");
-            inputScript("http://cdn.bootcss.com/leaflet.draw/0.4.12/leaflet.draw.js");
+            inputCSS("https://cdn.bootcss.com/leaflet.draw/0.4.12/leaflet.draw.css");
+            inputScript("https://cdn.bootcss.com/leaflet.draw/0.4.12/leaflet.draw.js");
         }
         if (inArray(includes, 'leaflet.pm')) {
-            inputCSS("http://cdn.bootcss.com/leaflet.pm/0.16.0/leaflet.pm.min.css");
-            inputScript("http://cdn.bootcss.com/leaflet.pm/0.16.0/leaflet.pm.min.js");
+            inputCSS("https://cdn.bootcss.com/leaflet.pm/0.16.0/leaflet.pm.min.css");
+            inputScript("https://cdn.bootcss.com/leaflet.pm/0.16.0/leaflet.pm.min.js");
         }
     }
 
